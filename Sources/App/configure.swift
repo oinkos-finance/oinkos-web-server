a9410a81439d2475b94272ca656399b2b2dd6772
Samuel Nascimento <me@samnsc.com>
2024-11-18 13:01:46 -0300

    Migrate to SQLite database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,6 +1,6 @@
 import NIOSSL
 import Fluent
-import FluentPostgresDriver
+import FluentSQLiteDriver
 import Vapor
 import JWT
 
@@ -9,14 +9,7 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+    app.databases.use(.sqlite(.file("oinkos.sqlite")), as: .sqlite)
 
     let key = ES256PrivateKey()
     await app.jwt.keys.add(ecdsa: key)
