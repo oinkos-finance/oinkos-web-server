ea988d1b40856e537553179e94ddc8ae638b5bb3
Samuel Nascimento <me@samnsc.com>
2025-01-21 20:28:52 -0300

    create authenticated route to return the current user’s information

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -10,18 +10,31 @@ import Vapor
 struct UserController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let users = routes.grouped("users")
+
+        users.grouped(
+            UserToken.authenticator(), UserToken.guardMiddleware()
+        ).get(use: self.getCurrentUser)
         
         users.group(":userId") { user in
             user.get(use: self.getUser)
         }
     }
-    
+
     @Sendable
     func getUser(request: Request) async throws -> UserResponseDTO {
-        guard let user = try? await User.find(request.parameters.get(":userId"), on: request.db) else {
+        guard
+            let user = try? await User.find(
+                request.parameters.get(":userId"), on: request.db)
+        else {
             throw Abort(.notFound, reason: "User not found")
         }
-        
+
         return user.toResponseDTO()
+    }
+
+    @Sendable
+    func getCurrentUser(request: Request) async throws -> UserResponseDTO {
+        let userToken = try request.auth.require(UserToken.self)
+        return userToken.user.toResponseDTO()
     }
 }
