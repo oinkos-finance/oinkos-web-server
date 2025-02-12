fbbb75ff0ca3df967080e71a341cf3c00b9f3441
Samuel Nascimento <me@samnsc.com>
2025-02-11 23:55:02 -0300

    Remove public folder
    We are not going to be serving any static assets so the public folder can be removed

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,9 +6,6 @@ import JWT
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
     app.databases.use(.sqlite(.file("oinkos.sqlite")), as: .sqlite)
 
     let key = ES256PrivateKey()
