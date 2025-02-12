b03bcd3e902aa536cb7edd15f1017dc7a295ab6a
Samuel Nascimento <me@samnsc.com>
2025-02-11 23:42:46 -0300

    Remove user information retrieval route

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -8,28 +8,12 @@
 import Vapor
 
 struct UserController: RouteCollection {
-    func boot(routes: RoutesBuilder) throws {
+    func boot(routes: RoutesBuilder) {
         let users = routes.grouped("users")
 
         users.grouped(
             UserToken.authenticator(), UserToken.guardMiddleware()
         ).get(use: self.getCurrentUser)
-        
-        users.group(":userId") { user in
-            user.get(use: self.getUser)
-        }
-    }
-
-    @Sendable
-    func getUser(request: Request) async throws -> UserResponseDTO {
-        guard
-            let user = try? await User.find(
-                request.parameters.get(":userId"), on: request.db)
-        else {
-            throw Abort(.notFound, reason: "User not found")
-        }
-
-        return user.toResponseDTO()
     }
 
     @Sendable
