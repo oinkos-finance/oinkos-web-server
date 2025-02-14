38a842bab6b4f56707f3bef7a37395e8d10ab430
Samuel Nascimento <me@samnsc.com>
2025-02-14 19:18:26 -0300

    Permissive CORS configuration

--- Sources/App/entrypoint.swift
+++ Sources/App/entrypoint.swift
@@ -23,6 +23,14 @@ enum Entrypoint {
             metadata: ["success": .stringConvertible(executorTakeoverSuccess)]
         )
 
+        let corsConfiguration = CORSMiddleware.Configuration(
+            allowedOrigin: .all,
+            allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+            allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+        )
+        let cors = CORSMiddleware(configuration: corsConfiguration)
+        app.middleware.use(cors, at: .beginning)
+        
         do {
             try await configure(app)
         } catch {
