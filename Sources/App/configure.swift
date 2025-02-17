1391b944da8c1e30860a62574c0376dcfd485ad0
Samuel Nascimento <me@samnsc.com>
2025-02-17 11:11:41 -0300

    Add unique transaction migration to app migrations

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,11 +4,12 @@ import JWT
 import NIOSSL
 import Vapor
 
-// configures your application
 public func configure(_ app: Application) async throws {
     app.databases.use(.sqlite(.file("oinkos.sqlite")), as: .sqlite)
 
-    guard let keyString = Environment.get("PRIVATE_KEY") else { fatalError("Could not find PRIVATE_KEY environment variable") }
+    guard let keyString = Environment.get("PRIVATE_KEY") else {
+        fatalError("Could not find PRIVATE_KEY environment variable")
+    }
 
     let key = try ES256PrivateKey(pem: keyString)
 
@@ -16,6 +17,7 @@ public func configure(_ app: Application) async throws {
 
     app.migrations.add(CreateUser())
     app.migrations.add(CreatePaymentType())
+    app.migrations.add(CreateUniqueTransaction())
     app.migrations.add(CreateRecurringTransaction())
 
     try routes(app)
