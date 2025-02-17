7176e4acac44d1905520ec4ccf989e9dc35284bc
Samuel Nascimento <me@samnsc.com>
2025-02-17 10:47:54 -0300

    Re-add guard middlewares

--- Sources/App/Controllers/LogInController.swift
+++ Sources/App/Controllers/LogInController.swift
@@ -10,23 +10,30 @@ import Vapor
 struct LogInController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let logIn = routes.grouped("login")
-        
-        logIn.post(use: self.getToken)
+
+        logIn.grouped(
+            User.authenticator(),
+            User.guardMiddleware(
+                throwing: Abort(.unauthorized, reason: "Unauthorized"))
+        )
+        .post(use: self.getToken)
     }
-    
+
     @Sendable
     func getToken(request: Request) async throws(Abort) -> UserTokenResponse {
         guard let user = try? request.auth.require(User.self) else {
             throw Abort(.unauthorized, reason: "Unauthorized")
         }
-        
+
         guard
             let token = try? user.generateToken(),
             let signedToken = try? await request.jwt.sign(token)
         else {
-            throw Abort(.internalServerError, reason: "Unable to generate authentication token for user")
+            throw Abort(
+                .internalServerError,
+                reason: "Unable to generate authentication token for user")
         }
-        
+
         return .init(token: signedToken)
     }
 }
--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -5,22 +5,30 @@
 //  Created by Sam Nascimento on 16/11/24.
 //
 
-import Vapor
 import Fluent
 import JWT
+import Vapor
 
 struct UserController: RouteCollection {
     func boot(routes: RoutesBuilder) {
         let user = routes.grouped("user")
 
-        user.get(use: self.getCurrentUser)
+        user.grouped(
+            UserToken.authenticator(),
+            UserToken.guardMiddleware(
+                throwing: Abort(.unauthorized, reason: "Unauthorized"))
+        )
+        .get(use: self.getCurrentUser)
     }
 
     @Sendable
-    func getCurrentUser(request: Request) async throws(Abort) -> UserResponseDTO {
+    func getCurrentUser(request: Request) async throws(Abort) -> UserResponseDTO
+    {
         guard
             let userToken = try? request.auth.require(UserToken.self),
-            let user = try? await User.query(on: request.db).filter(\.$id == userToken.userId).first()
+            let user = try? await User.query(on: request.db).filter(
+                \.$id == userToken.userId
+            ).first()
         else {
             throw Abort(.unauthorized, reason: "Unauthorized")
         }
